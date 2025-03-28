30cf3ca9c1bb24eb6f056476e3f86010a37bb970
chamara-g <chamara95.eng@gmail.com>
2025-03-28 10:18:06 +0530

    URL validator added

--- internal/handler/scraper_handler.go
+++ internal/handler/scraper_handler.go
@@ -5,12 +5,13 @@ import (
 	"net/http"
 
 	"github.com/gihanc.dev/web-scraper-app/internal/services"
+	"github.com/gihanc.dev/web-scraper-app/internal/utils"
 )
 
 func GetWebHTMLByURL(w http.ResponseWriter, r *http.Request) {
 
 	url := r.URL.Query().Get("url")
-	if url == "" {
+	if url == "" || !utils.IsValidURL(url) {
 		http.Error(w, "Invalid URL", http.StatusBadRequest)
 		return
 	}
